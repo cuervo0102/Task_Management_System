618b4628a34d635d41f0ed4eaa8ca6ac80bf024a
Saikaisiham <blackbutlersiham2001@gmail.com>
2023-12-28 22:40:19 +0100

    commit

--- log_register/models.py
+++ log_register/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    image = models.ImageField(upload_to='users_image')  
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)      
+
+    def __str__(self):
+        return str(self.user)
--- log_register/tests/test_views.py
+++ log_register/tests/test_views.py
@@ -1,6 +1,7 @@
+import json
 from django.test import TestCase, Client
 from django.urls import reverse
-import json
+from django.contrib.auth.models import User
 
 
 
@@ -9,6 +10,7 @@ class TestViews(TestCase):
         self.client = Client()
         self.register = reverse('register_request')
         self.login = reverse('login_request')
+        self.test = reverse('test')
 
 
         
@@ -19,8 +21,39 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'user_templates/register.html')
 
 
+    def test_register_POST(self):
+        user_data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123',
+        }
+        response = self.client.post(self.register, data=user_data)
+        print(response)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
     def test_login_GET(self):
         response = self.client.get(self.login)
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'user_templates/login.html')
+
+
+    # def test_login_POST(self):
+    #     login_data = {
+    #         'username': 'testuser',
+    #         'password': 'testpassword123',
+    #     }
+
+    #     response = self.client.post(self.login, data=login_data, follow=True)
+
+
+    #     print(f"Response content: {response.content}")
+    #     print(f"User authenticated: {response.wsgi_request.user.is_authenticated}")
+
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertRedirects(response, self.test)
+
+
+
